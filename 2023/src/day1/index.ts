13f675be678f4485667975051ab8f5ae9e693777
Joren Rothman <joren@instance.studio>
2023-12-01 15:40:29 +0100

    WIP day 1 part 2

--- 2023/src/day1/index.ts
+++ 2023/src/day1/index.ts
@@ -1,3 +1,4 @@
+import { match } from 'assert';
 import { Day } from '../day';
 
 class Day1 extends Day {
@@ -5,17 +6,79 @@ class Day1 extends Day {
         super(1);
     }
 
+    findNumbers(string: string) {
+        const numberStrings = [
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven',
+            'eight',
+            'nine',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+        ];
+
+        const numberMap: { [k: string]: string } = {
+            one: '1',
+            two: '2',
+            three: '3',
+            four: '4',
+            five: '5',
+            six: '6',
+            seven: '7',
+            eight: '8',
+            nine: '9',
+        };
+
+        let matches = numberStrings.map((numberString) => {
+            const index = string.indexOf(numberString);
+
+            if (index === -1) {
+                return {
+                    number: '100000000',
+                    index: 1000000,
+                };
+            }
+
+            if (numberString.length > 1) {
+                numberString = numberMap[numberString];
+            }
+
+            return {
+                number: numberString,
+                index,
+            };
+        });
+
+        matches.sort((a, b) => {
+            return a?.index - b?.index;
+        });
+
+        const result = matches.filter((match) => match.number !== '100000000');
+
+        return result.map(({ number }) => number);
+    }
+
     solveForPartOne(input: string): string {
         const expression = /\d+/g;
-
-        const regex = new RegExp(expression);
 
         const lines = input.split('\n');
 
         const numbers = lines.map((value) => value.match(expression));
+        let sum = 0;
 
-        const correctNumbers = numbers.map((number) => {
-            if (!number) {
+        numbers.forEach((number) => {
+            if (!number || number.length < 1) {
                 return 0;
             }
 
@@ -43,18 +106,58 @@ class Day1 extends Day {
 
             num = Number.parseInt(first + last);
 
-            return num;
+            sum = sum + num;
         });
 
-        const result = correctNumbers.reduce((prev, val) => {
-            return prev + val;
-        }, 0);
-
-        return `${result}`;
+        return `${sum}`;
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const lines = input.split('\n');
+
+        const numbers = lines.map((value) => this.findNumbers(value));
+
+        let sum = 0;
+
+        numbers.forEach((number) => {
+            if (!number) {
+                return 0;
+            }
+
+            if (number.length < 1) {
+                return 0;
+            }
+
+            let first = '';
+            let last = '';
+
+            let num = 0;
+
+            if (number.length < 2) {
+                first = number[0];
+                last = number[0];
+            } else {
+                first = number[0];
+
+                last = number[number.length - 1];
+            }
+
+            if (first.length > 1) {
+                first = first[0];
+            }
+
+            if (last.length > 1) {
+                last = last[last.length - 1];
+            }
+
+            num = Number.parseInt(first + last);
+
+            sum = sum + num;
+        });
+
+        console.log({ sum });
+
+        return `${sum}`;
     }
 }
 
